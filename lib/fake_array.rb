cd638c8af4706b669cc07df7370251be2f2d5877
elainapolson <elainapolson@gmail.com>
2015-09-10 11:54:04 -0400

    done

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -2,6 +2,27 @@ class FakeArray
 
   # you'll need a splat in this class somewhere
 
+  attr_accessor :array
 
+  def initialize(*arguments)
+    self.array = []
+    arguments.each do |a|
+      self.array << a 
+    end
+  end
+
+  def [](argument)
+    self.array[argument]
+  end
+
+  def each
+    self.array.each do |item|
+      puts item
+    end
+  end
+
+  def first
+    self.array.first
+  end
 
 end
